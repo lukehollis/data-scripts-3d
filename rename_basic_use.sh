3182abc74383acb9bcb8acd69c3af25a69e3f196
Luke Hollis <lukehollis@gmail.com>
2024-02-03 00:18:05 -0800

    up

--- rename_basic_use.sh
+++ rename_basic_use.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Directory containing the files. Replace '/path/to/directory' with the actual directory path.
-DIR="./eqs_orig"
+DIR="./"
 
 # Iterate over every file ending with (1).jpg in the specified directory
-for file in "$DIR"/*\(1\).jpg; do
+for file in "$DIR"/*\(1\).mp4; do
   # Check if the file exists to avoid errors in case of no matching files
   if [ -f "$file" ]; then
     # Generate new file name by removing (1)
-    new_name="${file%(1).jpg}.jpg"
+    new_name="${file%(1).mp4}.mp4"
     
     # Rename the file
     mv "$file" "$new_name"
