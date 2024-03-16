4ba7b9d070f71aa87146add4f4120ff0fadad612
Luke Hollis <lukehollis@gmail.com>
2024-03-16 15:27:53 -0700

    up

--- process_data.sh
+++ process_data.sh
@@ -6,19 +6,22 @@ for file in *
 do
 	fname=${file/.mp4}
 	fname=${fname/.mov}
-  ffmpeg -i $file -pix_fmt bgr8 images/"$fname"_frame_%05d.jpg  
+  ffmpeg -i $file -r 2 -pix_fmt bgr8 images/"$fname"_frame_%05d.jpg  
 done
 
 # downscale them
-python ../downscale_images.py
+# python downscale_images.py
 
 # extract features
-colmap feature_extractor --database_path colmap/database.db --image_path images --ImageReader.single_camera 1 --ImageReader.camera_model --SiftExtraction.use_gpu 0
+# colmap feature_extractor --database_path colmap/database.db --image_path images --ImageReader.single_camera 1 --ImageReader.camera_model OPENCV 
+colmap feature_extractor --database_path colmap/database.db --image_path images --ImageReader.single_camera 1 --ImageReader.camera_model OPENCV --SiftExtraction.use_gpu 0
 
 # match images
-# colmap vocab_tree_matcher --database_path ./colmap/database.db --SiftMatching.use_gpu --VocabTreeMatching.vocab_tree_path ../vocab_tree.fbow
+colmap vocab_tree_matcher --database_path ./colmap/database.db --VocabTreeMatching.vocab_tree_path ../vocab_tree.fbow
+colmap vocab_tree_matcher --database_path ./colmap/database.db --SiftMatching.use_gpu 0 --VocabTreeMatching.vocab_tree_path ../vocab_tree.fbow
 # sequential for video
 colmap sequential_matcher --database_path ./colmap/database.db --SiftMatching.use_gpu 0 
+# colmap exhaustive_matcher --SiftMatching.guided_matching=true --database_path ./colmap/database.db 
 
 # mapper 
 colmap mapper --database_path ./colmap/database.db --image_path images --output_path ./colmap/sparse --Mapper.ba_global_function_tolerance 1e-6
@@ -27,6 +30,7 @@ colmap mapper --database_path ./colmap/database.db --image_path images --output_
 colmap bundle_adjuster --input_path colmap/sparse/0 --output_path colmap/sparse/0 --BundleAdjustment.refine_principal_point 1
 
 # make transform
-python ../make_transforms.py
+python make_transforms.py
 
 # ready to train
+# ns-train nerfacto --viewer.websocket-port 7007 nerfstudio-data --data .
