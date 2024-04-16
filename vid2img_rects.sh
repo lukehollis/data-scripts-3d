d5601448ae1c2c883343ecb95eb7da64bf7ea89c
Luke Hollis <lukehollis@gmail.com>
2024-04-15 23:02:51 -0700

    up

---
+++ vid2img_rects.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create the "rects" directory if it doesn't exist
+mkdir -p rects
+
+# Loop through all .mov files in the "videos" directory
+for video in videos/*.mov; do
+  # Extract the filename without the extension
+  filename=$(basename "$video" .mov)
+  
+  # Create a directory for the current video frames if it doesn't exist
+  mkdir -p "rects/$filename"
+  
+  # Use ffmpeg to export frames as JPG images
+  ffmpeg -i "$video" -vf fps=1 "rects/$filename/$filename%04d.jpg"
+done
